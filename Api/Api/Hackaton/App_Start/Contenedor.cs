9dac52a07d131a28c5735429b6510521f830b32b
GRUPOFARSIMAN\mfuentes <maynor.fuentes@farsiman.com>
2018-08-24 13:06:06 -0600

    Agregando UnityContainer

--- Api/Api/Hackaton/App_Start/Contenedor.cs
+++ Api/Api/Hackaton/App_Start/Contenedor.cs
@@ -2,6 +2,7 @@
 using Hackaton.Domain.Core.Repositorios;
 using Hackaton.Infraestructure;
 using Hackaton.Infraestructure.Core;
+using Microsoft.Practices.Unity;
 using Ninject;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,8 @@ namespace Hackaton.App_Start
     public class Contenedor
     {
 
-        public static IKernel Kernel { get; set; }
+        //public static IKernel Kernel { get; set; }
+        public static UnityContainer UnityContainer;
 
         static Contenedor()
         {
@@ -26,19 +28,28 @@ namespace Hackaton.App_Start
 
         public static void RegistrarDependencias(IKernel kernel)
         {
-           kernel.Bind(typeof(IRepositorio<>)).To(typeof(Repositorio<>));
-            kernel.Bind<IUnitOfWork, IEntityUnitOfWork>().To<HackatonUnitOfWork>()
-                .WithConstructorArgument("repositorioType", x => { return typeof(Repositorio<>); });
+            //kernel.Bind(typeof(IRepositorio<>)).To(typeof(Repositorio<>));
+            // kernel.Bind<IUnitOfWork, IEntityUnitOfWork>().To<HackatonUnitOfWork>()
+            //     .WithConstructorArgument("repositorioType", x => { return typeof(Repositorio<>); });
 
-            kernel.Bind<IHackatonService>().To<HackatonService>();
-            //kernel.Bind<IReporteParaleloProcedimientoAppService>().To<ReporteParaleloProcedimientoAppService>();
-            //kernel.Bind<ISQLServerManagementAppService>().To<SQLServerManagementAppService>();
-            Kernel = kernel;
+            // kernel.Bind<IHackatonService>().To<HackatonService>();
+            // //kernel.Bind<IReporteParaleloProcedimientoAppService>().To<ReporteParaleloProcedimientoAppService>();
+            // //kernel.Bind<ISQLServerManagementAppService>().To<SQLServerManagementAppService>();
+            // Kernel = kernel;
+
+            UnityContainer.RegisterType<IEntityUnitOfWork, HackatonUnitOfWork>();
+            UnityContainer.RegisterType(typeof(IRepositorio<>), typeof(Repositorio<>));
+            UnityContainer.RegisterType<IHackatonService, HackatonService>();
         }
 
-        public static T Get<T>()
+        //public static T Get<T>()
+        //{
+        //    return Kernel.Get<T>();
+        //}
+
+        public static TObjeto Resolve<TObjeto>()
         {
-            return Kernel.Get<T>();
+            return UnityContainer.Resolve<TObjeto>();
         }
 
     }
